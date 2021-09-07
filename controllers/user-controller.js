b9663b526dee2fafa3868fdbe780fde04e9ef98d
Victor Lupascu <sinajeen@gmail.com>
2021-09-07 17:23:32 -0500

    add update user method

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -14,6 +14,47 @@ const userController = {
         res.status(400).json(err)
       })
   },
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: 'thoughts',
+        select: '-__v',
+      })
+      .select('-__v')
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' })
+          return
+        }
+        res.json(dbUserData)
+      })
+      .catch((err) => {
+        console.log(err)
+        res.status(400).json(err)
+      })
+  },
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err)
+        res.status(400).json(err)
+      })
+  },
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' })
+          return
+        }
+        res.json(dbUserData)
+      })
+      .catch((err) => res.status(400).json(err))
+  },
 }
 
 module.exports = userController
