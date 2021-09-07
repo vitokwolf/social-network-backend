c73fe7ab1a7b014287c3ede82786026c75190ba8
Victor Lupascu <sinajeen@gmail.com>
2021-09-07 17:25:50 -0500

    add remove friend method

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -95,6 +95,22 @@ const userController = {
       })
       .catch((err) => res.status(400).json(err))
   },
+  removeFriend({ params }, res) {
+    User.findByIdAndUpdate(
+      { _id: params.id },
+      { $pull: { friends: params.friendId } },
+      { new: true, runValidators: true },
+    )
+      .select('-__v')
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No friend found with this id!' })
+          return
+        }
+        res.json(dbUserData)
+      })
+      .catch((err) => res.status(400).json(err))
+  },
 }
 
 module.exports = userController
